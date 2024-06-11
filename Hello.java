5a4fc842017f3ca32f778f2365f4f94559b785cf
SupriseSelinda <selindasurprise22@gmail.com>
2024-06-11 06:58:22 +0200

    Fisrt

---
+++ Hello.java
@@ -0,0 +1,11 @@
+public class Hello{
+
+   public static void  main(String []args){
+         
+        String name="Suprise";
+        
+        System.out.println("Hello"+" "+name );
+
+    }
+
+}
